f69eddc5f8bd0a27f833a5433440530b0009ae8e
talharahman <talharahman@pursuit.org>
2020-07-11 14:16:11 -0400

    nav to add fragment

--- app/src/main/java/com/example/habitstacks/view/HabitDashboard.kt
+++ app/src/main/java/com/example/habitstacks/view/HabitDashboard.kt
@@ -4,22 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.habitstacks.R
+import com.example.habitstacks.database.HabitDatabase
+import com.example.habitstacks.databinding.HabitDashboardBinding
+import com.example.habitstacks.viewmodel.DashBoardViewModelFactory
+import com.example.habitstacks.viewmodel.HabitDashboardViewModel
+import androidx.navigation.findNavController
 
 class HabitDashboard : Fragment() {
 
-    private val add: FloatingActionButton? = null
-    private val habitCard: ViewPager? = null
-    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+        val binding: HabitDashboardBinding = DataBindingUtil.inflate(
+                inflater, R.layout.habit_dashboard, container, false)
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = HabitDatabase.getInstance(application).habitDao
+        val viewModelFactory = DashBoardViewModelFactory(dataSource, application)
+        val dashBoardViewModel = ViewModelProvider(
+                this, viewModelFactory).get(HabitDashboardViewModel::class.java)
+
+        binding.habitDashboardViewModel = dashBoardViewModel
+
+        binding.newHabitButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_dashBoard_to_addNewHabit)
+
+        }
+
+
+        return binding.root
     }
 
-    companion object {
-        fun newInstance(): HabitDashboard {
-            return HabitDashboard()
-        }
-    }
+
 }
--- app/src/main/java/com/example/habitstacks/viewmodel/HabitDashboardViewModel.kt
+++ app/src/main/java/com/example/habitstacks/viewmodel/HabitDashboardViewModel.kt
@@ -1,5 +1,45 @@
 package com.example.habitstacks.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.habitstacks.database.HabitDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
-class HabitDashboardViewModel : ViewModel()
+class HabitDashboardViewModel(
+        dataSource: HabitDao,
+        application: Application) : ViewModel() {
+
+    private val dataBase = dataSource
+
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val _showSelectedHabit = MutableLiveData<Long>()
+    val showSelectedHabit get() = _showSelectedHabit
+
+    val habits = dataBase.getAllHabits()
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
+
+
+class DashBoardViewModelFactory(
+        private val dataSource: HabitDao,
+        private val application: Application
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HabitDashboardViewModel::class.java)) {
+            return HabitDashboardViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
